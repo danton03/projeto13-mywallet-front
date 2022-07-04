b5bb20c9753a59d7d92959c29c302c9ced47e172
Danton Matheus <dantoncosta03@hotmail.com>
2022-07-04 14:33:37 -0300

    :white_check_mark: Conclui o projeto

---
+++ src/components/AddExpensePage.jsx
@@ -0,0 +1,101 @@
+import { ContainerPage, Header } from "./layouts/HomePageStyles";
+import UserContext from "../contexts/UserContext";
+import { useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { ContainerAuth } from "./layouts/ContainerAuth";
+import { ButtonSubmit } from "./layouts/ButtonSubmit";
+
+export default function AddExpensePage() {
+  const navigate = useNavigate();
+  const { user } = useContext(UserContext);
+  const [formData, setFormData] = useState({
+    amount: '',
+    description: ''
+  });
+  const [isDisabled, setIsDisabled] = useState(false)
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsDisabled(true);
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user.token}`
+      }
+    }
+    const promise = axios.post("http://localhost:5000/addExpense", formData, config);
+    promise.then((response) => {
+      setIsDisabled(false);
+      navigate("/home");
+    });
+    promise.catch((error) => {
+      const status = error.response.status;
+      if (status === 401) {
+        navigate("/");
+      }
+      setIsDisabled(false);
+    })
+
+    toast.promise(
+      promise,
+      {
+        pending: 'Carregando...',
+        success: 'Saída adicionada com sucesso!',
+        error: {
+          render({ data }) {
+            const code = data.response.status;
+            //Verificar se esses são os códigos corretos
+            if (code === 401 || code === 422) {
+              const message = data.response.data;
+              return message;
+            }
+            else {
+              return "Ops, tivemos uma falha interna";
+            }
+          }
+        }
+      }
+    );
+  }
+
+  return (
+    <ContainerPage>
+      <Header>
+        <p>Nova saída</p>
+      </Header>
+
+      <ContainerAuth>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="number"
+            placeholder="Valor"
+            step="0.01"
+            min="0.01"
+            value={formData.amount}
+            onChange={(e) => { setFormData({ ...formData, amount: e.target.value }) }}
+            disabled={isDisabled}
+            required
+          />
+
+          <input
+            type="text"
+            maxLength={20}
+            placeholder="Descrição"
+            value={formData.description}
+            onChange={(e) => { setFormData({ ...formData, description: e.target.value }) }}
+            disabled={isDisabled}
+            required
+          />
+
+          <ButtonSubmit
+            type="submit"
+            disabled={isDisabled}
+          >
+            Salvar saída
+          </ButtonSubmit>
+        </form>
+      </ContainerAuth>
+    </ContainerPage>
+  );
+}
